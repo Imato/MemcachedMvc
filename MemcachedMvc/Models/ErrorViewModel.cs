62a1197fb083cac0cb16ca36a10a9be8daafd3f9
Imato <alexey.varentsov@gmail.com>
2019-02-21 22:41:53 +0300

    Add project files.

---
+++ MemcachedMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MemcachedMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
